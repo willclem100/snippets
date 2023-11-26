a0fe42a8a9a977be173b64ce1873d87bf0342f05
willclem100 <will.clem@biteninja.com>
2023-11-25 20:27:45 -0600

    Update to lesson 30

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id: parseInt(props.params.id),
+    },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
+}
