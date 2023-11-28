2e2d42ac6e12aab63961e6e3368820d4597b46ce
willclem100 <will.clem@biteninja.com>
2023-11-27 19:31:12 -0600

    Update to lesson 44 (error handling)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { db } from "@/db";
+import { type } from "os";
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
@@ -13,5 +14,34 @@ export async function editSnippet(id: number, code: string) {
 
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
+  redirect("/");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  //Check the user's inputs and make sure they're valid
+  const title = formData.get("title"); //title comes from the name attribute on the input in the form below
+  const code = formData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { message: "Title must be longer" };
+  }
+
+  if (typeof code !== "string" || code.length < 10) {
+    return { message: "Code must be longer" };
+  }
+
+  //Create a new record in the database
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  //Redirect the user back to the root route
+
   redirect("/");
 }
