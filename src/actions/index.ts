d1ce19310e0fe52b6043b47ead316a04766eea3f
willclem100 <will.clem@biteninja.com>
2023-12-05 20:34:53 -0600

    Update to lesson 45 (caching)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,26 +21,35 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  //Check the user's inputs and make sure they're valid
-  const title = formData.get("title"); //title comes from the name attribute on the input in the form below
-  const code = formData.get("code");
+  try {
+    //Check the user's inputs and make sure they're valid
+    const title = formData.get("title"); //title comes from the name attribute on the input in the form below
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return { message: "Title must be longer" };
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "Title must be longer" };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return { message: "Code must be longer" };
+    }
+
+    //Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
-
-  if (typeof code !== "string" || code.length < 10) {
-    return { message: "Code must be longer" };
-  }
-
-  //Create a new record in the database
-  // const snippet = await db.snippet.create({
-  //   data: {
-  //     title,
-  //     code,
-  //   },
-  // });
-  throw new Error("Ooops!!");
 
   //Redirect the user back to the root route
 
